9010196901f22da14f9b775f727b75a15ffd2821
aahel <aahel.guha@hashicorp.com>
2024-05-27 16:21:21 +0530

    fmt tf

--- tests/acceptance/elasticache-redis-cluster-auto-backup-enabled/cases/all-resources-complaint/main.tf
+++ tests/acceptance/elasticache-redis-cluster-auto-backup-enabled/cases/all-resources-complaint/main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 resource "aws_elasticache_cluster" "example" {
-  cluster_id           = "cluster-example"
-  engine               = "redis"
-  node_type            = "cache.m4.large"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
-  port                 = 6379
-  snapshot_retention_limit    = 7
+  cluster_id               = "cluster-example"
+  engine                   = "redis"
+  node_type                = "cache.m4.large"
+  num_cache_nodes          = 1
+  parameter_group_name     = "default.redis3.2"
+  engine_version           = "3.2.10"
+  port                     = 6379
+  snapshot_retention_limit = 7
 }
 
 resource "aws_elasticache_replication_group" "example" {
