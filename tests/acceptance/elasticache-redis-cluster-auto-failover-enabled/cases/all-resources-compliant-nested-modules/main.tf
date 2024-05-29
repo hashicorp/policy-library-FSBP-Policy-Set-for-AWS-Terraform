46faa58101be128f09e85d98e2d31feb165f4729
aahel <aahel.guha@hashicorp.com>
2024-05-29 18:20:58 +0530

    added elasticache failover poilcy

---
+++ tests/acceptance/elasticache-redis-cluster-auto-failover-enabled/cases/all-resources-compliant-nested-modules/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+
+module "elasticache_replication_group" {
+  source                   = "./elasticache_replication_group_backup_enabled"
+  replication_group_id     = "tf-rep-group-1"
+  snapshot_retention_limit = 7
+}
