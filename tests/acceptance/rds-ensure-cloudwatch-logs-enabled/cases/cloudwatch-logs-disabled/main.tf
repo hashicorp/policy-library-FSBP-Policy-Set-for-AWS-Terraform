5711fa6cc95fa55511992fe6f8691910fa5a8b7e
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-06-24 21:12:13 +0530

    fix disabled

--- tests/acceptance/rds-ensure-cloudwatch-logs-enabled/cases/cloudwatch-logs-disabled/main.tf
+++ tests/acceptance/rds-ensure-cloudwatch-logs-enabled/cases/cloudwatch-logs-disabled/main.tf
@@ -1,9 +1,9 @@
 resource "aws_db_instance" "this" {
-  allocated_storage   = 10
-  instance_class      = "db.t3.micro"
-  engine              = "mysql"
-  deletion_protection = false
-  storage_encrypted   = false
+  allocated_storage               = 10
+  instance_class                  = "db.t3.micro"
+  engine                          = "mysql"
+
+  storage_encrypted = true
 }
 
 provider "aws" {
